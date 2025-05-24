a1ab4774adc638beb36e9be4a2ef4a4c4a62f03e
Pierre <pierre.pbesse@gmail.com>
2025-05-24 16:44:33 +0200

    planet rotator modif

--- Assets/PlanetRotator.cs
+++ Assets/PlanetRotator.cs
@@ -8,14 +8,15 @@ public class PlanetRotator : MonoBehaviour
     public GameObject planetParent;
 
 
-    public void Rotate(float x, float y, float rotate_speed)
+    public void Rotate(float x, float y, float z, float rotate_speed)
     {   Vector3 planetRot = planetParent.transform.eulerAngles;
         rotator.transform.rotation = Quaternion.Euler(0, 0, 0);
         planetParent.transform.eulerAngles = planetRot;
         
         var rotEulers = rotator.transform.eulerAngles;
-        rotEulers.z += y * rotate_speed * Time.deltaTime;
         rotEulers.x += x * rotate_speed * Time.deltaTime;
+        rotEulers.y += y * rotate_speed * Time.deltaTime;
+        rotEulers.z += z * rotate_speed * Time.deltaTime;
         rotator.transform.eulerAngles = rotEulers;
     }
 }
