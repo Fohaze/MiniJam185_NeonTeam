489dca171fc148cc34d1d0bfa016e8be44dfbaff
= <adri12tenapro@gmail.com>
2025-05-25 18:41:59 +0200

    fix planette / camera gestion (détéction de la hauteur par rapport au centre de la planette (ground < 30 (1er étage) 32 < (2éme étage) (camera_sys (variable "current_center_dist"))

--- Assets/camera_sys_i1.cs
+++ Assets/camera_sys_i1.cs
@@ -9,6 +9,10 @@ public class camera_sys_i1 : MonoBehaviour
     public alien_ctrl1 alien_ctrl;
     public float stage_1_height = -76f;
     public float stage_2_height = -70f;
+    public float current_dist = 1f;
+    public float current_center_dist = 1f;
+    public GameObject center;
+    public GameObject perso;
 
     public void set_top_view()
     {
@@ -21,11 +25,14 @@ public class camera_sys_i1 : MonoBehaviour
 
     void Update()
     {
+        current_dist = perso.transform.position.y - player.transform.position.y;
+        current_center_dist = perso.transform.position.y - center.transform.position.y;
+
         if (alien_ctrl == null)
         {
             alien_ctrl = GameObject.FindObjectOfType<alien_ctrl1>();
         }
-        if (alien_ctrl != null && alien_ctrl.current_height > stage_1_height)
+        if (alien_ctrl != null &&  current_center_dist> stage_1_height)
         {
             anim.SetBool("stage_1_cam", true);
         }
@@ -33,7 +40,7 @@ public class camera_sys_i1 : MonoBehaviour
         {
             anim.SetBool("stage_1_cam", false);
         }
-        if (alien_ctrl != null && alien_ctrl.current_height > stage_2_height)
+        if (alien_ctrl != null && current_center_dist > stage_2_height)
         {
             anim.SetBool("stage_2_cam", true);
         }
