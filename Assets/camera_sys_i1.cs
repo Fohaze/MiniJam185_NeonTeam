656f8e3eb0595060698495d415b745f943808106
= <adri12tenapro@gmail.com>
2025-05-26 04:31:22 +0200

    fixe_ infinity falling

--- Assets/camera_sys_i1.cs
+++ Assets/camera_sys_i1.cs
@@ -13,6 +13,8 @@ public class camera_sys_i1 : MonoBehaviour
     public float current_center_dist = 1f;
     public GameObject center;
     public GameObject perso;
+    public dead_sys dead;
+    public float fall_dist;
 
     public void set_top_view()
     {
@@ -25,13 +27,26 @@ public class camera_sys_i1 : MonoBehaviour
 
     void Update()
     {
+
         current_dist = perso.transform.position.y - player.transform.position.y;
         current_center_dist = perso.transform.position.y - center.transform.position.y;
+
+
 
         if (alien_ctrl == null)
         {
             alien_ctrl = GameObject.FindObjectOfType<alien_ctrl1>();
         }
+        
+        if (alien_ctrl != null &&  perso.transform.position.y < center.transform.position.y)
+        {
+            dead.Respawn();
+            var a = perso.transform.position;
+            //perso.transform.position = center.transform.position.y + stage_2_height;
+            a.y = center.transform.position.y + stage_2_height;
+            perso.transform.position = a;
+        }
+
         if (alien_ctrl != null &&  current_center_dist> stage_1_height)
         {
             anim.SetBool("stage_1_cam", true);
